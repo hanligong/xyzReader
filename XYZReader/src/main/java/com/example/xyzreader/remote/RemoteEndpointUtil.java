8d485967858bc25596822b842a3620c30042fa9c
hanyuezi <hanyuezi@finupgroup.com>
2018-01-30 16:52:06 +0800

    c

---
+++ XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
@@ -0,0 +1,89 @@
+package com.example.xyzreader.remote;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class RemoteEndpointUtil {
+    private static final String TAG = "RemoteEndpointUtil";
+
+    private RemoteEndpointUtil() {
+    }
+
+    public static JSONArray fetchJsonArray() {
+        String itemsJson = null;
+        try {
+            itemsJson = fetchPlainText(Config.BASE_URL);
+        } catch (IOException e) {
+            Log.e(TAG, "Error fetching items JSON", e);
+            return null;
+        }
+
+        // Parse JSON
+        try {
+            JSONTokener tokener = new JSONTokener(itemsJson);
+            Object val = tokener.nextValue();
+            if (!(val instanceof JSONArray)) {
+                throw new JSONException("Expected JSONArray");
+            }
+            return (JSONArray) val;
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parsing items JSON", e);
+        }
+
+        return null;
+    }
+
+    public static JSONArray getAssetsData(Context context){
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            //获取assets资源管理器
+            AssetManager assetManager = context.getAssets();
+            //通过管理器打开文件并读取
+            BufferedReader bf = new BufferedReader(new InputStreamReader(
+                    assetManager.open("data.txt")));
+            String line;
+            while ((line = bf.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Parse JSON
+        try {
+            JSONTokener tokener = new JSONTokener(stringBuilder.toString());
+            Object val = tokener.nextValue();
+            if (!(val instanceof JSONArray)) {
+                throw new JSONException("Expected JSONArray");
+            }
+            return (JSONArray) val;
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parsing items JSON", e);
+        }
+        return null;
+    }
+
+    static String fetchPlainText(URL url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+}
